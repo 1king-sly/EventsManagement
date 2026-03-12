5768ee50f62bf133c2211987b5751f34322b8363
Byrone Kinsly <Kinslybyrone17@gmail.com>
2026-03-12 06:04:04 +0300

    Added user model.

---
+++ EventsManagement/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace EventsManagement.Models
+{
+    public class User(Guid userId, string firstName,string lastName,string email,string password,string profileImage,string? institutionId,string? schoolId)
+    {
+        public Guid UserId { get; set; } = userId;
+        public string FirstName { get; set; } = firstName;
+        public string LastName { get; set; } = lastName;
+        public string  Email { get; set; } = email;
+
+        public string Password { get; set; } = password;
+        public string ProfileImage { get; set; } = profileImage;
+        public string? InstitutionId { get; set; }=institutionId;
+        public string? SchoolId {  get; set; }=schoolId; 
+    }
+}
