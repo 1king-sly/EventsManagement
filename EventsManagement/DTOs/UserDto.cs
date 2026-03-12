e6f741843ca872d7cdd3d90f2c7e42f08c293484
Byrone Kinsly <Kinslybyrone17@gmail.com>
2026-03-12 10:14:51 +0300

    implemeted the user dto for all possible scenarios as of now

---
+++ EventsManagement/DTOs/UserDto.cs
@@ -0,0 +1,35 @@
+﻿using EventsManagement.Models;
+
+namespace EventsManagement.DTOs
+{
+    public class UserLoginDto(string email,string password):UserBase(email,password)
+    {
+    }
+
+    public class UserCreateDto(string firstName, string lastName,string email,string password) : UserBase(email, password)
+    {
+        public string FirstName { get; set; } = firstName;
+        public string LastName { get; set; } = lastName;
+    }
+
+    public class UserOutDto(Guid userId, string firstName,string lastName,string email,string? profileImage)
+    {
+        public Guid UserId { get; set; } = userId;
+        public string FirstName { get; set; } = firstName;
+        public string LastName { get; set; } = lastName;
+        public string Email { get; set; } = email;
+        public string? ProfileImage { get; set; } = profileImage;
+
+    }
+
+    public class UserTokenDto(string refreshToken, string accessToken)
+    {
+        public string RefreshToken { get; set; } = refreshToken;
+        public string AccessToken { get; set; } = accessToken;  
+    }
+
+    public class UserLoginOutDto(Guid userId, string firstName, string lastName, string email, string? profileImage,string refreshToken,string accessToken) : UserOutDto(userId, firstName, lastName, email, profileImage)
+    {
+        public UserTokenDto Token { get; set; } = new(refreshToken,accessToken);
+    }
+}
